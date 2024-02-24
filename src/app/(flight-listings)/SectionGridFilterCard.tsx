e56dc498754c594f9d9617c1cf009ac15b92df5d
NdeNifty <Ndekong@gmail.com>
2024-02-24 15:53:04 +0100

    deleted the the package.lock

--- src/app/(flight-listings)/SectionGridFilterCard.tsx
+++ src/app/(flight-listings)/SectionGridFilterCard.tsx
@@ -76,7 +76,7 @@ const SectionGridFilterCard: FC<SectionGridFilterCardProps> = ({
       data-nc-id="SectionGridFilterCard"
     >
       <Heading2
-        heading="Singapore - Tokyo"
+        heading="New York - Douala"
         subHeading={
           <span className="block text-neutral-500 dark:text-neutral-400 mt-3">
             22 flights
@@ -86,7 +86,7 @@ const SectionGridFilterCard: FC<SectionGridFilterCardProps> = ({
           </span>
         }
       />
-      <div className="mb-8 lg:mb-11">
+      <div className="mb-4 lg:mb-4 lg:-mt-10">
         <TabFilters />
       </div>
       <div className="lg:p-10 lg:bg-neutral-50 lg:dark:bg-black/20 grid grid-cols-1 gap-6  rounded-3xl">
