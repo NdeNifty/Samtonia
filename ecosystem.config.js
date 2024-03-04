7e39844d49a0e10164625135708cdfc6ed0bc0fc
NdeNifty <Ndekong@gmail.com>
2024-03-04 16:43:45 +0100

    ecosystem.config file added 3

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,6 +3,7 @@ module.exports = {
       name: 'samtoniatravels',
       script: 'npm',
       args: 'run start', // Assumes your package.json has a "start" script for Next.js
+      cwd: '/root/actions-runner/_work/Samtonia/Samtonia',
       watch: ['.'], // Watch all files for development; consider limiting this in production
       ignore_watch: ['node_modules', '.next'], // Ignore watching node_modules and .next build directory
       env: {
