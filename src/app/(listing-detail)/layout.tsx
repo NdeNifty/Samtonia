f0c592f42621c87c668c137679c854bfd8616447
NdeNifty <Ndekong@gmail.com>
2024-03-08 09:34:58 +0100

    removed other listings 9

--- src/app/(listing-detail)/layout.tsx
+++ src/app/(listing-detail)/layout.tsx
@@ -7,9 +7,9 @@ import SectionSubscribe2 from "@/components/SectionSubscribe2";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import React, { ReactNode } from "react";
 import MobileFooterSticky from "./(components)/MobileFooterSticky";
-import { imageGallery as listingStayImageGallery } from "./listing-stay-detail/constant";
-import { imageGallery as listingCarImageGallery } from "./listing-car-detail/constant";
-import { imageGallery as listingExperienceImageGallery } from "./listing-experiences-detail/constant";
+// import { imageGallery as listingStayImageGallery } from "./listing-stay-detail/constant";
+// import { imageGallery as listingCarImageGallery } from "./listing-car-detail/constant";
+// import { imageGallery as listingExperienceImageGallery } from "./listing-experiences-detail/constant";
 import { Route } from "next";
 
 const DetailtLayout = ({ children }: { children: ReactNode }) => {
@@ -24,27 +24,27 @@ const DetailtLayout = ({ children }: { children: ReactNode }) => {
     router.push(`${thisPathname}/?${params.toString()}` as Route);
   };
 
-  const getImageGalleryListing = () => {
-    if (thisPathname?.includes("/listing-stay-detail")) {
-      return listingStayImageGallery;
-    }
-    if (thisPathname?.includes("/listing-car-detail")) {
-      return listingCarImageGallery;
-    }
-    if (thisPathname?.includes("/listing-experiences-detail")) {
-      return listingExperienceImageGallery;
-    }
+  // const getImageGalleryListing = () => {
+  //   if (thisPathname?.includes("/listing-stay-detail")) {
+  //     return listingStayImageGallery;
+  //   }
+  //   if (thisPathname?.includes("/listing-car-detail")) {
+  //     return listingCarImageGallery;
+  //   }
+  //   if (thisPathname?.includes("/listing-experiences-detail")) {
+  //     return listingExperienceImageGallery;
+  //   }
 
-    return [];
-  };
+  //   return [];
+  // };
 
   return (
     <div className="ListingDetailPage">
-      <ListingImageGallery
+      {/* <ListingImageGallery
         isShowModal={modal === "PHOTO_TOUR_SCROLLABLE"}
         onClose={handleCloseModalImageGallery}
         images={getImageGalleryListing()}
-      />
+      /> */}
 
       <div className="container ListingDetailPage__content">{children}</div>
 
