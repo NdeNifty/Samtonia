e7480343ef370a12edde8c875e68814a9a38679a
NdeNifty <Ndekong@gmail.com>
2024-03-08 08:51:55 +0100

    removed other listings 5

--- src/app/(server-components)/SectionHeroArchivePage.tsx
+++ src/app/(server-components)/SectionHeroArchivePage.tsx
@@ -8,7 +8,7 @@ import Image, { StaticImageData } from "next/image";
 export interface SectionHeroArchivePageProps {
   className?: string;
   listingType?: ReactNode;
-  currentPage: "Stays" | "Experiences" | "Cars" | "Flights";
+  currentPage:  "Flights";
   currentTab: SearchTab;
   rightImage?: StaticImageData;
 }
